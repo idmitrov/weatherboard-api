f7c4abe2bba95f172879a46836a694ab04e2af12
Dimitar <mailto.dimitrov@gmail.com>
2019-11-23 18:05:36 +0200

    Init Api and add Run method

---
+++ api.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+
+module.exports = {
+	/**
+	 * Run the API on a given Host and Port
+	 * @name run
+	 * @param {Number|String} port The port to use (default=5000)
+	 * @param {String} host The host to use (default=localhost)
+	 */
+	run(port = 5000, host = 'localhost') {
+		const api = new Koa();
+
+		api.listen(port, host);
+	}
+}
